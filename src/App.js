c33b33dc4b86166a2155b32d7febef001b2f3bbe
Md Arifuzzaman <45159402+mdarifuzzaman11@users.noreply.github.com>
2022-02-09 07:28:24 -0500

    testing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './App.css';
-import MessengerCustomerChat from 'react-messenger-customer-chat';
+//import MessengerCustomerChat from 'react-messenger-customer-chat';
 
-function App() {
-  return (
-    <MessengerCustomerChat
-    pageId="524371708070058"
-    appId="2272918056294793"
+//function App() {
+  //return (
+    //<MessengerCustomerChat
+    //pageId="524371708070058"
+    //appId="2272918056294793"
     
-  />
-  );
-}
+  ///>
+  //);
+//}
 
 export default App;
 
